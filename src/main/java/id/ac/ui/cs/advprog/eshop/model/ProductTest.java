561f016aa4b3282effcb6595d2b2092c6c8aabcd
akmalnabil22 <akmaln761@gmail.com>
2025-02-13 18:38:31 +0700

    add unit test getter

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    Product product;
+    @BeforeEach
+    void setUp() {
+        this.product = new Product();
+        this.product.setProductId("04651c0a-3304-4ff5-b670-0641ab2f92f3");
+        this.product.setProductName("Sampo Cap Bambang");
+        this.product.setProductQuantity(100);
+    }
+    @Test
+    void testGetProductId() {
+        assertEquals("04651c0a-3304-4ff5-b670-0641ab2f92f3", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName() {
+        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100, this.product.getProductQuantity());
+    }
+}
